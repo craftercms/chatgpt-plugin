db6505b58423fad5446713f6c2fe1ef9e8d9d5e1
Phil Nguyen <nquangphuong@gmail.com>
2025-01-13 15:33:00 +0700

    Revert change on main.tsx

--- sources/src/main.tsx
+++ sources/src/main.tsx
@@ -1,12 +1,19 @@
 import React, { useMemo, useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import { Box, createTheme, IconButton, InputAdornment, TextField, Tooltip, useMediaQuery } from '@mui/material';
+import {
+  Box,
+  createTheme,
+  GlobalStyles,
+  IconButton,
+  InputAdornment,
+  TextField,
+  ThemeProvider,
+  Tooltip,
+  useMediaQuery
+} from '@mui/material';
 import Tiny from './Tiny.tsx';
 import OpenAILogo from './OpenAI';
 import ChatGPTPromptPopover from './ChatGPTPromptPopover';
-import GlobalStyles from '@craftercms/studio-ui/components/GlobalStyles';
-import CrafterThemeProvider from '@craftercms/studio-ui/components/CrafterThemeProvider';
-import I18nProvider from '@craftercms/studio-ui/components/I18nProvider';
 
 function AppWrap() {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
@@ -22,38 +29,42 @@ function AppWrap() {
   const [value, setValue] = useState('Skydiving risks');
   const [anchorEl, setAnchorEl] = useState(null);
   return (
-    <CrafterThemeProvider themeOptions={theme}>
-      <I18nProvider>
-        <Box sx={{ p: 5 }}>
-          <TextField
-            fullWidth
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-            sx={{ mb: 3 }}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <Tooltip title="Send message">
-                    <IconButton onClick={(e) => setAnchorEl(e.target)}>
-                      <OpenAILogo />
-                    </IconButton>
-                  </Tooltip>
-                </InputAdornment>
-              )
-            }}
-          />
-          <ChatGPTPromptPopover
-            open={Boolean(anchorEl)}
-            anchorEl={anchorEl}
-            context={value}
-            onClose={() => setAnchorEl(null)}
-            print={(content) => setValue(content)}
-          />
-          <Tiny />
-          <GlobalStyles cssBaseline={true} />
-        </Box>
-      </I18nProvider>
-    </CrafterThemeProvider>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ p: 5 }}>
+        <TextField
+          fullWidth
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          sx={{ mb: 3 }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <Tooltip title="Send message">
+                  <IconButton onClick={(e) => setAnchorEl(e.target)}>
+                    <OpenAILogo />
+                  </IconButton>
+                </Tooltip>
+              </InputAdornment>
+            )
+          }}
+        />
+        <ChatGPTPromptPopover
+          open={Boolean(anchorEl)}
+          anchorEl={anchorEl}
+          context={value}
+          onClose={() => setAnchorEl(null)}
+          print={(content) => setValue(content)}
+        />
+        <Tiny />
+        <GlobalStyles
+          styles={{
+            'body, html': { margin: 0, padding: 0, height: '100%' },
+            body: { background: theme.palette.background.default },
+            '#root': { height: '100%' }
+          }}
+        />
+      </Box>
+    </ThemeProvider>
   );
 }
 
